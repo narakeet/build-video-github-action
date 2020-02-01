79752cff89822cccbdf3c1cf8efa79d45c2c909a
Gojko Adzic <gojko@gojko.com>
2020-02-01 21:02:45 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -2,25 +2,24 @@
 const core = require('@actions/core'),
 	apiRequest = require('minimal-request-promise'),
 	run = async function () {
-		try {
-			const apiUrl = core.getInput('api-url'),
-				event = JSON.stringify({
-					source: core.getInput('source-path'),
-					repository: process.env['GITHUB_REPOSITORY'],
-					token: core.getInput('github-token'),
-					repositoryType: 'github',
-					sha: process.env['GITHUB_SHA']
-				}),
-				response = await apiRequest.post(apiUrl, {
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify(event)
-				});
-			console.log('got result', response.body);
-		} catch (error) {
-			console.error(error);
-			core.setFailed(error.message || error);
-		}
+		const apiUrl = core.getInput('api-url'),
+			event = JSON.stringify({
+				source: core.getInput('source-path'),
+				repository: process.env['GITHUB_REPOSITORY'],
+				token: core.getInput('github-token'),
+				repositoryType: 'github',
+				sha: process.env['GITHUB_SHA']
+			}),
+			response = await apiRequest.post(apiUrl, {
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(event)
+			});
+		console.log('got result', response.body);
 	};
-run();
+run()
+	.catch(e => {
+		console.error(e);
+		core.setFailed(e.message || e);
+	});
